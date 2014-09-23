5e8f4bb22c41065bc9f99105f46faf860715024b
Takashi Kokubun <takashi-kokubun@cookpad.com>
2014-09-23 14:54:17 +0900

    fix error

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -84,6 +84,8 @@ class Isucon2App < Sinatra::Base
          ORDER BY order_id DESC LIMIT 10',
       ).to_a
 
+      return [] if recent_sold.size == 0
+
       values = recent_sold.map { |data|
         %Q{('#{data["seat_id"]}',#{data["order_id"] ? data["order_id"] : "NULL" },'#{data["a_name"]}','#{data["t_name"]}','#{data["v_name"]}')}
       }.join(",")
